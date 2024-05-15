646bcd281691b674c8bb5e15cc7bac89fada0dea
Venkata Kakumani <venkata@govtechpros.com>
2024-05-15 15:41:41 -0400

    I updated the code with MySQL DB using XAMPP

--- hello.py
+++ hello.py
@@ -1,20 +1,33 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type' 
+CORS(app, origins="http://192.168.56.1:3000")
+app.config['CORS_HEADERS'] = 'Content-Type' 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'kvsp_paul'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Welcome to authentication api!!</p>"
 
-@app.route("/login", methods=['GET', 'POST'])
-# @cross_origin()
-def login():
+@app.route("/signup", methods=['GET', 'POST'])
+@cross_origin()
+def signup():
     if request.method == 'POST':
-        request.form.get('username')
-        request.form.get('password')
+        username = request.json.get('name')
+        email = request.json.get('email')
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO pygo (name, email) VALUES (%s,%s)", (username,email))
+        mysql.connection.commit()
+        cursor.close()
+        print(username)
+        print(email)
         return jsonify({'status': "post success"})
     else:
          return jsonify({'status': "get success"})
@@ -22,4 +35,4 @@ def login():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
